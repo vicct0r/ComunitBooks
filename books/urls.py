46188106c2e095e3547f01a77075251f2fe3982b
vicct0r <v1ctor_inacio@proton.me>
2025-10-25 17:22:41 -0300

    fix (books): Fixed User Library/Library views

--- books/urls.py
+++ books/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'books'
 
 urlpatterns = [
-    path('', views.UserBookListView.as_view(), name='list'),
+    path('', views.AllBookListView.as_view(), name='library'),
     path('add/', views.BookCreateView.as_view(), name='create'),
     path('<int:pk>/<slug:slug>/', views.UserBookDetailView.as_view(), name='detail'),
     path('library/user/<int:pk>/', views.UserBookListView.as_view(), name='user_library')
--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
 
 from .models import Book
 
@@ -20,16 +21,19 @@ class BookCreateView(LoginRequiredMixin, generic.CreateView):
         return reverse('books:user_library', kwargs={'pk': self.request.user.pk})
 
 
+class AllBookListView(generic.ListView):
+    template_name = 'books/library.html'
+    model = Book
+    context_object_name = 'books'
+
+
 class UserBookListView(generic.ListView):
-    template_name = 'books/users_books.html'
+    template_name = 'books/user_books.html'
     model = Book
     context_object_name = 'books'
 
     def get_queryset(self):
-        user_id = self.kwargs.get('id')
-        if user_id:
-            return Book.objects.filter(owner=user_id)
-        return Book.objects.all()
+        return Book.objects.filter(owner=self.kwargs.get('pk'))
 
 
 class UserBookDetailView(generic.DetailView):
